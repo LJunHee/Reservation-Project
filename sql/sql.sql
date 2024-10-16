19423a6ee2741192ba9890fd8ac499723c48296b
LJunHee <wnsgml4334@naver.com>
2024-10-16 18:01:51 +0900

    Revert "bean, restDao, restVo, controller"
    This reverts commit 16e100b826f020fc275647ebca5b29788a248c67.

--- sql/sql.sql
+++
@@ -1,6 +0,0 @@
-reservationprojectrestaurantCREATE TABLE restaurant (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    restName VARCHAR(100) NOT NULL,
-    restReview TEXT,
-    restTime DATETIME NOT NULL UNIQUE
-);
