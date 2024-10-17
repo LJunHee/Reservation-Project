18368aab25775121b5e062ec65f6b0c0593b29b3
LJunHee <wnsgml4334@naver.com>
2024-10-17 11:03:37 +0900

    sql 수정

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,6 +1,13 @@
-reservationprojectrestaurantCREATE TABLE restaurant (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    restName VARCHAR(100) NOT NULL,
-    restReview TEXT,
-    restTime DATETIME NOT NULL UNIQUE
-);
+CREATE TABLE `restaurant` (
+	`restNo` INT NOT NULL AUTO_INCREMENT,
+	`restName` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`restStar` INT NOT NULL DEFAULT '5',
+	`restReview` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`restTime` DATETIME NULL DEFAULT NULL,
+	PRIMARY KEY (`restNo`) USING BTREE,
+	UNIQUE INDEX `restTime` (`restTime`) USING BTREE
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=4
+;
