28fbde64a8a07f0413278919a5090c7865336015
LJunHee <wnsgml4334@naver.com>
2024-10-17 09:14:07 +0900

    test

--- src/main/java/com/team2/reservation/HomeController.java
+++ src/main/java/com/team2/reservation/HomeController.java
@@ -11,10 +11,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+<<<<<<< HEAD
 @Controller
 public class HomeController {
 <<<<<<< HEAD
 =======
+=======
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+>>>>>>> parent of 16e100b (bean, restDao, restVo, controller)
 
 	@GetMapping("/")
 	public String index(Model model) {
