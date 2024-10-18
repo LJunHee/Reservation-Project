8895fa6128881068a0cedfe4451f0ec940a34bee
LJunHee <wnsgml4334@naver.com>
2024-10-18 15:57:03 +0900

    중복 이메일 체크 기능 추가

--- src/main/java/com/team2/reservation/user/model/UserDao.java
+++ src/main/java/com/team2/reservation/user/model/UserDao.java
@@ -1,5 +1,6 @@
 package com.team2.reservation.user.model;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -7,9 +8,15 @@ import org.apache.ibatis.annotations.Update;
 @Mapper
 public interface UserDao {
 	
+	//로그인 확인
 	@Select(value = "select * from users where userEmail = #{userEmail}")
 	UserVo chklogin(String userEmail);
 	
-	@Update(value = "insert into users (userName,userEmail,userPw,userPhone) values (#{userName},#{userEmail},#{userPw},#{userPhone})")
+	//중복 이메일 확인
+	@Select(value = "select count(*) from users where userEmail = #{userEmail}")
+	int countByEmail(String userEmail);
+	
+	//회원가입
+	@Insert(value = "insert into users (userName,userEmail,userPw,userPhone) values (#{userName},#{userEmail},#{userPw},#{userPhone})")
 	int addInfo(UserVo bean);
 }
